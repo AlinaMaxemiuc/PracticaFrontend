c86f5ed42f86b9935e7566949b50a98bdd6f46fa
Alina Maxemiuc <alina.maxemiuc6@gmail.com>
2023-07-28 13:20:40 +0300

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
 import home from "../assets/home.css";
 export default function Home() {
+  useEffect(()=>{
+    fetch('http://localhost:3001/get')
+    .then(res=>{
+      let data=res.json();
+      console.log(data);    
+    });
+    
+  })
   return(
     <div class="bgd">
   <div class="homePage">Coming soon</div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,100 +3,128 @@ import { useNavigate } from "react-router-dom";
 import login from "../assets/login.css";
 
 export default function Login() {
-
   const [username, setUsername] = useState("");
-  const [password, setPassword]= useState("");
-  const[checkPass, setCheckPass]=useState(false);
-  const navigate=useNavigate();
-  const[errorMessages,setErrorMessages]=useState({});
-  const[isSubmitted,setIsSubmitted]=useState(false);
+  const [password, setPassword] = useState("");
+  const [checkPass, setCheckPass] = useState(false);
+  const navigate = useNavigate();
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const database=[
+  const database = [
     {
       username: "user1@gmail.com",
-      password: "password1"
+      password: "password1",
     },
     {
-      username:"user2@gmail.com",
-      password:"password2"
-    }
+      username: "user2@gmail.com",
+      password: "password2",
+    },
   ];
-  const errors={
-    errUser:"Invalid username",
-    errPass:"Invalid password"
-  }
-  const handleSubmit=(e)=>
-  {
+
+  const errors = {
+    errUser: "Invalid username",
+    errPass: "Invalid password",
+  };
+
+  //verificare email si parola pt user si afiseaza eroare in caz ca nu e bn
+  const handleSubmit = (e) => {
     e.preventDefault();
-    var {errUser,errPass}=document.forms[0];
+    var { errUser, errPass } = document.forms[0];
 
-    const userData = database.find((user)=>user.username==errUser.value);
+    const userData = database.find((user) => user.username == errUser.value);
 
-    if(userData)
-    {
-      if(userData.password!=errPass.value){
-        setErrorMessages({name:"errPass", message: errors.errPass});
-      }else{
+    if (userData) {
+      if (userData.password != errPass.value) {
+        setErrorMessages({ name: "errPass", message: errors.errPass });
+      } else {
         setIsSubmitted(true);
       }
-    }else{
-      setErrorMessages({name:"errUser", message: errors.errUser});
+    } else {
+      setErrorMessages({ name: "errUser", message: errors.errUser });
     }
   };
 
-  const errMessages=(e)=>e===errorMessages.name &&(<div className="error">{errorMessages.message}</div>);
+  //eroare afisata pe un div/ecran
+  const errMessages = (e) =>
+    e === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
 
-
-  function navigateToHome(e){
+  //navigare catre pagina de home  
+  function navigateToHome(e) {
     e.preventDefault();
-    navigate('/home');
+    navigate("/home");
   }
 
-  const togglePassword=(e)=>{
+  //pentru checkBox =>arata sau ascunde parola
+  const togglePassword = (e) => {
     setCheckPass(e.target.checked);
-  }
- 
-  function validateForm()
-  {
-    return username.length>6 && password.length>6;
+  };
+
+  //validare form
+  function validateForm() {
+    return username.length > 6 && password.length > 6;
   }
 
-  function Username(user)
-  {
+  //setare username (imi ramane salvat intr-un "pop-up" email-utile deja introduse)
+  function Username(user) {
     setUsername(user.target.value);
   }
-  function Password (pass)
-  {
+  //parola
+  function Password(pass) {
     setPassword(pass.target.value);
   }
+
+  //functie care creeaza un obiect {data} luat din baza de date
+  //se "conecteaza" la adresa data si aplica metoda de post pentru a trimite raspunsul cerut
+  function login(e) {
+    let data = { name: "Apetrei Madalina", password: "parola" };
+    fetch("http://localhost:3001/login", { method: "POST", body: data }).then(
+      (res) => {
+        let data = res.json();
+        console.log(data);
+       // navigateToHome(e);
+      }
+    );
+  }
   return (
- 
     <div class="form-login">
-      {/* <h1 class="hLogin">Login</h1> */}
-    <form action="" onSubmit={(e)=>navigateToHome(e)}>
-      <div class="login">
-        {/* <p>Username</p> */}
-        <input type="email" class="form-control" name="errUser" placeholder="Email" onChange={Username}/>
-        {/* <label for ="floatingInput">Email adress</label> */}
-        {errMessages("errUser")}
+      <form action="" onSubmit={login}>
+        <div class="login">
+          <input
+            type="email"
+            class="form-control"
+            name="errUser"
+            placeholder="Email"
+            onChange={Username}
+          />
+          {errMessages("errUser")}
         </div>
         <div class="login">
-        {/* <p >Password</p> */}
-        <input type={checkPass ? "text": "password"} class="form-control" name="errPass" placeholder="Password" onChange={Password}/>
-        {/* <label for="floatingPassword">Password</label> */}
-        {errMessages("errPass")}
-      </div>
-      <div class="showPass"> 
-        <label for="showPass" >Show password?</label>
-        <input type="checkbox" 
-        onChange={(e=>{togglePassword(e)})}
-        //  checked={togglePassword}
-         /></div>
-      <div>
-        <button class="btn" type="submit" disabled={!validateForm()} >Log in</button>
-      </div>
-    </form>
+          <input
+            type={checkPass ? "text" : "password"}
+            class="form-control"
+            name="errPass"
+            placeholder="Password"
+            onChange={Password}
+          />
+          {errMessages("errPass")}
+        </div>
+        <div class="showPass">
+          <label for="showPass">Show password?</label>
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              togglePassword(e);
+            }}
+          />
+        </div>
+        <div>
+          <button class="btn" type="submit" disabled={!validateForm()}>
+            Log in
+          </button>
+        </div>
+      </form>
     </div>
-
   );
 }
